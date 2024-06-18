50235f923fee2643003b652f5c6d9bcc0c0424e5
Luiz Fernando Nunes Silva <164936897+Luizfernando170@users.noreply.github.com>
2024-06-18 02:21:34 +0000

    eu fiz alterações na pasta

--- Trabalho/Trabalho.js
+++ Trabalho/Trabalho.js
@@ -4,3 +4,6 @@ sr.reveal('.area-1', {duration: 1000});
 sr.reveal('.area-2', {duration: 1000});
 sr.reveal('.area-3', {duration: 1000});
 sr.reveal('.area-4', {duration: 1000});
+sr.reveal('.area-5', {duration: 1000});
+sr.reveal('.area-6', {duration: 1000});
+sr.reveal('.area-7', {duration: 1000});
